4e291045ccd4d6bbd600c8da894d4278107993e1
pxm77460 <pxm77460@ucmo.edu>
2023-05-01 15:16:31 -0500

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDgtg8brbHCFWe7ytybcp_TXPase0-wzEU",
+    authDomain: "crypto-hunter-ef693.firebaseapp.com",
+    projectId: "crypto-hunter-ef693",
+    storageBucket: "crypto-hunter-ef693.appspot.com",
+    messagingSenderId: "192662090197",
+    appId: "1:192662090197:web:f44f97cd915b2fdd308d5c"
+  };
+
+export default firebaseConfig;
